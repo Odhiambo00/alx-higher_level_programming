7d7d63d5fbc73d7ddbf0b4a63e282ea3ca5778ed
Odhiambo00 <odhiamboelias99@gmail.com>
2023-05-14 12:10:32 +0300

    created files with solutions for task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        mod_list = my_list.copy()
+        mod_list[idx] = element
+        return mod_list
