716344482afddc86caf559fb0a88476ae57ccc0c
Odhiambo00 <odhiamboelias99@gmail.com>
2023-05-17 18:48:30 +0000

    updated 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,4 +8,4 @@ returns a key with the biggest integer value
 def best_score(a_dictionary):
     if not a_dictionary:
         return
-    return max(a_dictionary, key = a_dictionary.get)
+    return (max(a_dictionary, key=a_dictionary.get))
