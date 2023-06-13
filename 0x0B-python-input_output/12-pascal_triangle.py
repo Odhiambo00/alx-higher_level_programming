97930768c60a19aa6071d33a3fb010980da6cc56
Odhiambo00 <odhiamboelias99@gmail.com>
2023-06-13 22:07:07 +0300

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,14 +11,26 @@ def pascal_triangle(n):
     """
 
     if n <= 0:
-        return []
+        return list()
 
-    triangles = [[1]]
-    when len(traingles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
+    triangles = []
+    for line in range(0, n):
+        tmp = []
+        for i in range(0, line + 1):
+            tmp.append(magic(line, i))
         triangles.append(tmp)
     return triangles
+
+
+def magic(n, k):
+    """
+    Function magic
+    """
+
+    res = 1
+    if (k > n - k):
+        k = n - k
+    for i in range(0, k):
+        res *= (n - i)
+        res = res // (i + 1)
+    return res
