514bdd91ce610e8bbfe86f4ae0e375f4bb35fe7d
Odhiambo00 <odhiamboelias99@gmail.com>
2023-06-13 21:35:51 +0300

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+class Student
+"""
+
+
+class Student:
+    """
+    Represents a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of student instance
+        """
+        return self.__dict__
