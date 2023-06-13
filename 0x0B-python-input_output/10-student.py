a9d5ac52559034492aae4d7494fd5f2fc821b874
Odhiambo00 <odhiamboelias99@gmail.com>
2023-06-13 22:24:03 +0300

    10-student.py update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -26,10 +26,9 @@ class Student:
 
         if attrs is None:
             return self.__dict__
+
         new_dict = {}
         for a in attrs:
-            try:
+            if a in self.__dict__:
                 new_dict[a] = self.__dict__[a]
-            except:
-                pass
         return new_dict
