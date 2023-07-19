f971b2abe5ac6ebd826d4ce4e67b457f6b420e90
Odhiambo00 <odhiamboelias99@gmail.com>
2023-07-19 19:00:44 +0300

    10-genre_id_by_show.sql update

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- lists all shows
-SELECT show.title, show_gen.genre_id FROM tv_shows AS shows
-INNER JOIN tv_show_genres ON shows.id = show_gen.show_id
-ORDER BY shows.title, show_gen.genre_id;
+SELECT tv_shows.title, tv_show_gen.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_gen.show_id
+ORDER BY tv_shows.title, tv_show_gen.genre_id;
